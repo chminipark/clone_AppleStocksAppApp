67238c8c975706843cb629f47dca503f33760d0f
minii <chminipark@gmail.com>
2022-01-27 17:10:51 +0900

    first commit

---
+++ clone_AppleStocksApp/clone_AppleStocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  clone_AppleStocksAppApp.swift
+//  clone_AppleStocksApp
+//
+//  Created by minii on 2022/01/27.
+//
+
+import SwiftUI
+
+@main
+struct clone_AppleStocksAppApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
